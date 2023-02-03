dab085f9bca5d8b41c1f4c003113d585685d83ff
Caroline Miller <caroline.brendle.miller@gmail.com>
2023-02-03 14:28:24 -0600

    adding temp_table_notes - updated

--- temp_tables_notes.sql
+++ temp_tables_notes.sql
@@ -42,6 +42,82 @@ set named = 'BIG'
 where n >=4
 ;
 
+-- verify the update
 select * from my_numbers;
+
+-- now we delete stuff
+delete from my_numbers
+where n = 2;
+
+-- verify
+select * from my_numbers; -- the row is now gone
+
+-- switch to employees DB 
+
+use employees;
+
+show tables;
+
+select first_name, last_name, salary, dept_name
+from employees
+join dept_emp as de using (emp_no)
+join salaries as s using (emp_no)
+join departments using (dept_no)
+where de.to_date > now()
+and s.to_date > now()
+and dept_name = 'customer service';
+
+-- now we want to turn this into a temp table
+
+create temporary table oneil_2099.curr_custserv_emps as -- save table in my DB eneil.2099
+(
+select first_name, last_name, salary, dept_name
+from employees
+join dept_emp as de using (emp_no)
+join salaries as s using (emp_no)
+join departments using (dept_no)
+where de.to_date > now()
+and s.to_date > now()
+and dept_name = 'customer service'
+)
+;
+
+-- Great!
+
+select * from oneil_2099.curr_custserv_emps;
+
+-- and it populates!
+
+use oneil_2099;
+
+drop table curr_custserv_emps;
+
+select database();
+use employees;
+
+select first_name, last_name, salary, dept_name
+from employees
+join dept_emp as de using (emp_no)
+join salaries as s using (emp_no)
+join departments using (dept_no)
+where de.to_date > now()
+and s.to_date > now()
+and dept_name = 'customer service'
+;
+
+
+create temporary table oneil_2099.curr_custserv_emps as -- save table in my DB eneil.2099
+(
+select first_name, last_name, salary, dept_name
+from employees
+join dept_emp as de using (emp_no)
+join salaries as s using (emp_no)
+join departments using (dept_no)
+where de.to_date > now()
+and s.to_date > now()
+and dept_name = 'customer service'
+)
+;
+
 
 
